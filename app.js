3fc7f149113de1d81b96c701b4b9ca65ca9504e6
Vidhya Goud <128922145+chitti-goud@users.noreply.github.com>
2024-03-05 19:36:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+ import React from "react";
+ import ReactDOM  from "react-dom/client";
+ 
+ const parent=React.createElement("div",{id:"parent"},
+              React.createElement("div",{id:"child"},
+              React.createElement("h1",{},"I am h1 tag")));
 
-    const heading=React.createElement("h1",{},"Hello From React");
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+              console.log(parent);
 
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
